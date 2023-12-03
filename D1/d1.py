826111b30892abfc7752eb6fcc0efb0507cd367a
Karol <karol@MacBook-Pro-Karol.local>
2023-12-03 10:51:24 +0100

    Refacotred a little bit first day

--- D1/d1.py
+++ D1/d1.py
@@ -1,34 +1,46 @@
 import re
 
 
-lines = None
-index = 1
-scores = []
-score = 0
+def main():
+    lines = None
+    score = 0
+    digits_as_literals = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ]
+    with open("input.txt") as f:
+        lines = f.readlines()
 
-with open('input.txt') as f:
-    lines = f.readlines()
+    for id, line in enumerate(lines):
+        print(line.strip())
+        number = "".join(c for c in line.strip() if c.isdigit())
 
-for id, line in enumerate(lines):
-    print(line.strip())
-    number = ''.join(c for c in line.strip() if c.isdigit())
+        score += int(number[0] + number[-1])
+    print(number[0] + number[-1])
 
-    score += int(number[0]+number[-1])
-print(number[0]+number[-1])
+    print("Score for part one is: " + str(score))
+
+    score = 0
+    for line in lines:
+        digits = []
+        for i, c in enumerate(line.strip()):
+            if c.isdigit():
+                digits.append(c)
+            else:
+                for d, val in enumerate(digits_as_literals):
+                    if line[i:].startswith(val):
+                        digits.append(str(d + 1))
+        score += int(digits[0] + digits[-1])
+
+    print("Score for part two is: " + str(score))
 
 
-print("Score for part one is: "+str(score))
-
-score=0
-for line in lines:
-    digits = []
-    for i,c in enumerate(line.strip()):
-        if c.isdigit():
-            digits.append(c)
-        else:
-            for d,val in enumerate(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']):
-                if line[i:].startswith(val):
-                    digits.append(str(d+1))
-    score+=int(digits[0]+digits[-1])
-
-print("Score for part two is: "+str(score))
+if __name__ == "__main__":
+    main()

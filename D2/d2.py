5b572091ce3cc410f9560e1b7d1a69f93706c260
Karol <karol@MacBook-Pro-Karol.local>
2023-12-03 10:39:54 +0100

    Refactoring of day2

--- D2/d2.py
+++ D2/d2.py
@@ -1,76 +1,61 @@
 import re
 
-MAX_BLUE = 14
-MAX_RED = 12
-MAX_GREEN = 13
+MAX_COLORS = {"blue": 14, "red": 12, "green": 13}
 
 
-def one_of_results_is_not_feasible(results: list) -> [bool, int, int, int]:
-    min_blues = 0
-    min_reds = 0
-    min_greens = 0
+def evaluate_results(results: list, min_values: dict) -> tuple:
+    not_feasible = False
+
     for result in results:
-        result = result.strip()
-        not_feasible = False
-        if "red" in result:
-            if int(result.split(" ")[0]) > MAX_RED:
-                not_feasible = True
-            if int(result.split(" ")[0]) > min_reds:
-                min_reds = int(result.split(" ")[0])
-        if "green" in result:
-            if int(result.split(" ")[0]) > MAX_GREEN:
-                not_feasible = True
-            if int(result.split(" ")[0]) > min_greens:
-                min_greens = int(result.split(" ")[0])
-        if "blue" in result:
-            if int(result.split(" ")[0]) > MAX_BLUE:
-                not_feasible = True
-            if int(result.split(" ")[0]) > min_blues:
-                min_blues = int(result.split(" ")[0])
-    return not_feasible, min_blues, min_greens, min_reds
+        value_str, color = result.strip().split(" ")
+        value = int(value_str)
+
+        if value > MAX_COLORS[color]:
+            not_feasible = True
+
+        if value > min_values[color]:
+            min_values[color] = value
+
+    return not_feasible, min_values["blue"], min_values["green"], min_values["red"]
 
 
-def is_game_feasible(draws: list) -> [bool, int, int, int]:
+def evaluate_game(draws: list) -> tuple:
     feasible = True
-    blues = 0
-    greens = 0
-    reds = 0
+    min_colors = {"blue": 0, "green": 0, "red": 0}
+
     for draw in draws:
         draw = draw.replace(", ", ",")
         results = draw.split(",")
-        not_feasible, min_blues, min_greens, min_reds = one_of_results_is_not_feasible(
-            results
+        not_feasible, min_blues, min_greens, min_reds = evaluate_results(
+            results, min_colors
         )
         feasible = not not_feasible
-        if min_blues > blues:
-            blues = min_blues
-        if min_greens > greens:
-            greens = min_greens
-        if min_reds > reds:
-            reds = min_reds
-    return feasible, blues, greens, reds
+
+        min_colors["blue"] = max(min_colors["blue"], min_blues)
+        min_colors["green"] = max(min_colors["green"], min_greens)
+        min_colors["red"] = max(min_colors["red"], min_reds)
+
+    return feasible, min_colors["blue"], min_colors["green"], min_colors["red"]
 
 
 def main():
-    lines = None
-    score = 0
-    score_2 = 0
+    score_part_1 = 0
+    score_part_2 = 0
 
     with open("input.txt") as f:
         lines = f.readlines()
 
     for id, line in enumerate(lines):
-        line = line.strip()
-        line = re.sub("Game ([1-9]|[1-9]\d|100):", "", line)
+        line = re.sub("Game ([1-9]|[1-9]\d|100):", "", line.strip())
         draws = line.split(";")
-        feasible, blues, greens, reds = is_game_feasible(draws)
+        feasible, blues, greens, reds = evaluate_game(draws)
         print(f"game:{id} r:{reds} g:{greens} b:{blues}")
         if feasible:
-            score += id + 1
-        score_2 += blues * greens * reds
+            score_part_1 += id + 1
+        score_part_2 += blues * greens * reds
 
-    print("Score of part 1 is " + str(score))
-    print("Score of part 2 is " + str(score_2))
+    print("Score of part 1 is " + str(score_part_1))
+    print("Score of part 2 is " + str(score_part_2))
 
 
 if __name__ == "__main__":
